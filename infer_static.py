d6bfa3c3395015c4472ace9c0d97296be5b3706a
Mufei Li <mufeili1996@gmail.com>
2025-10-04 17:04:07 -0400

    update

--- infer_static.py
+++ infer_static.py
@@ -1,5 +1,7 @@
 import os
 
+from utils.data import load_data
+
 # Set TOKENIZERS_PARALLELISM to false to avoid warnings with multiprocessing
 os.environ["TOKENIZERS_PARALLELISM"] = "false"
 
@@ -46,6 +48,8 @@ def main(args):
     os.makedirs(save_dir, exist_ok=True)
     
     out_file = os.path.join(save_dir, 'pred.jsonl')
+    
+    data_all = load_data()
 
 if __name__ == '__main__':
     from argparse import ArgumentParser
